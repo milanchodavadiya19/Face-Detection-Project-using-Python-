8d2b6e9313fcd78e0a25e912758bef86b4c93459
Milan Chodavadiya <120493459+milanchodavadiya19@users.noreply.github.com>
2023-10-15 22:03:10 +0200

    Update Facedetectionfile.py

--- Facedetectionfile.py
+++ Facedetectionfile.py
@@ -7,7 +7,6 @@ import cv2
 cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # data file
 
-
 cap = cv2.VideoCapture(0)
 # turn on the video (Web camera)
 # utilized VideoCapture functions
